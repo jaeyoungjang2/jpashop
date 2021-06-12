152f99bee87785b571cf0e7dd5996375bfde5dea
jaeyoungjang2 <lion85488@gmail.com>
2021-06-12 16:36:08 +0900

    Delivery domain 추가

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,2 +1,19 @@
-package jpabook.jpashop.domain;public class Delivery {
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Delivery {
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // Ready, Comp
 }
