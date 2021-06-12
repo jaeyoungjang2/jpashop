d1540429fabfeef986a44aa2c31621d71362bb2a
jaeyoungjang2 <lion85488@gmail.com>
2021-06-12 16:37:46 +0900

    OrderItem domain 추가

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,2 +1,65 @@
-package jpabook.jpashop.domain;public class OrderItem {
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.item.item;
+
+import javax.persistence.*;
+
+@Entity
+public class OrderItem {
+    @Id @GeneratedValue
+    @Column(name = "order_item")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice; // 주문 가격
+
+    private int count; // 주문 수량
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public jpabook.jpashop.domain.item.item getItem() {
+        return item;
+    }
+
+    public void setItem(jpabook.jpashop.domain.item.item item) {
+        this.item = item;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
