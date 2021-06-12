c7241f177e840dcd378dfe53a94146925b9d3c99
jaeyoungjang2 <lion85488@gmail.com>
2021-06-12 16:37:25 +0900

    Order domain 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,2 +1,25 @@
-package jpabook.jpashop.domain;public class Order {
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List orderItems;
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private OrderStatus status;
 }
