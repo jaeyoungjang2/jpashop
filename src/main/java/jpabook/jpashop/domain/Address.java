4bdf0b9f53d6377ac19a58a416821b450e8bab8f
jaeyoungjang2 <lion85488@gmail.com>
2021-06-19 12:34:54 +0900

    Adrees domain update

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -9,6 +9,15 @@ public class Address {
     private String street;
     private String zipcode;
 
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
     public String getCity() {
         return city;
     }
