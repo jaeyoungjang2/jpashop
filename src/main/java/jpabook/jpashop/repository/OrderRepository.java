124c7a8bf0174a6b20cb6cab9c1f6942a686ef95
jaeyoungjang2 <lion85488@gmail.com>
2021-06-19 12:49:25 +0900

    주문 내역 검색, 주문 상태 검색 기능 추가

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -15,12 +15,17 @@ public class OrderRepository {
     }
 
     // 주문 저장 //
-    public void save(Order order){
+    public void save(Order order) {
         em.persist(order);
     }
 
     // 주문 단건 조회 //
-    public Order findOne(Long id){
+    public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
+
+    // 주문 내역 검색, 주문 상태 검색 //
+    //    public List<Order> findAll(OrderSearch orderSearch){
+//
+//        }
 }
