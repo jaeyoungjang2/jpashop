802d3c58c4a45e6e9da2038fdfe6e90b0608cf86
jaeyoungjang2 <lion85488@gmail.com>
2021-06-19 12:38:43 +0900

    MemberrRepository 추가

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,2 +1,39 @@
-package jpabook.jpashop.repository;public class MemberRepository {
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public MemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long memberId){
+        return em.find(Member.class, memberId);
+    }
+
+    public List<Member> findAll(){
+
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
 }
