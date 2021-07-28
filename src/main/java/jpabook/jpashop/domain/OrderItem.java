457e32c75603b804065333b4f208aa6e8c302220
jaeyoungjang2 <lion85488@gmail.com>
2021-07-28 22:41:08 +0900

    feat: orderItem 추가

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -19,7 +19,6 @@ public class OrderItem {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
-
     private Order order;
     private int orderPrice; // 주문 당시 가격
     private int count; // 주문 당시 수량
